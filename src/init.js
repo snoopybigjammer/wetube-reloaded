1005ea783288f4adf193861bd99677719e7f1f86
jinsung kim <godidtntksqn@gmail.com>
2024-02-24 13:47:34 +0900

    PORT

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`sever is alive!, your server listening on ${PORT} `);
