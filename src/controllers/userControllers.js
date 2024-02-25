4530d89b061a2049f8ae0e5ee914b56730d91fe5
jinsung kim <godidtntksqn@gmail.com>
2024-02-25 13:22:30 +0900

    s3 upload

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -229,7 +229,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+
+const s3 = new aws.S3({
+  region: "ap-northeast-2",
+  accessKeyId: process.env.AWS_ID,
+  secretAccessKey: process.env.AWS_SECRET,
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: "snoopywetube",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "wetube";
@@ -28,9 +42,11 @@ export const publicOnlyMiddleware = (req, res, next) => {
 export const uploadAvatar = multer({
   dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
+  storage: multerUploader,
 });
 
 export const uploadVideo = multer({
   dest: "uploads/videos/",
   limits: { fileSize: 100000000 },
+  storage: multerUploader,
 });
